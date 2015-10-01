0dfdb74a7936be3e06b769f55af0e61e7d69e59e
arpunk <arpunk@cryptolab.net>
2015-09-30 19:52:13 -0500

    Use the public region API

--- test/geo/region_test.exs
+++ test/geo/region_test.exs
@@ -22,37 +22,37 @@ defmodule Geo.RegionTest do
 
     {:ok, region} = Region.new("Bogotá", 4.598056, -74.075833)
 
-    GenServer.call(region, {:add_object, "driver b", @point_b})
-    GenServer.call(region, {:add_object, "driver a", @point_a})
+    Region.add_object(region, "driver b", @point_b)
+    Region.add_object(region, "driver a", @point_a)
 
     {:ok, [region: region]}
   end
 
   test "Can store a new location", %{region: region} do
-    assert :ok = GenServer.call(region, {:add_object, "other driver", @point_a})
+    assert :ok = Region.add_object(region, "other driver", @point_a)
   end
 
   test "Can't add the same location twice", %{region: region} do
-    assert :ok = GenServer.call(region, {:add_object, "other driver", @point_a})
-    assert :already_added = GenServer.call(region, {:add_object, "other driver", @point_a})
+    assert :ok = Region.add_object(region, "other driver", @point_a)
+    assert :already_added = Region.add_object(region, "other driver", @point_a)
   end
 
   test "Can locate nearby objects", %{region: region} do
-    assert {:ok, [@point_a]} = GenServer.call(region, {:query_around, @booking_a, 500})
-    assert {:ok, [@point_b]} = GenServer.call(region, {:query_around, @booking_b, 1_000})
+    assert {:ok, [@point_a]} = Region.query_around(region, @booking_a, 500)
+    assert {:ok, [@point_b]} = Region.query_around(region, @booking_b, 1_000)
   end
 
   test "Stored objects", %{region: region} do
-    assert {:ok, drivers} = GenServer.call(region, :list_objects)
+    assert {:ok, drivers} = Region.list_objects(region)
     assert is_list(drivers)
     refute is_nil(drivers)
   end
 
   test "Delete an stored object", %{region: region} do
-    assert :ok = GenServer.call(region, {:remove_object, "driver a"})
+    assert :ok = Region.remove_object(region, "driver a")
   end
 
   test "Can't delete an unknown object", %{region: region} do
-    assert :not_found = GenServer.call(region, {:remove_object, "driver aa"})
+    assert :not_found = Region.remove_object(region, "driver aa")
   end
 end
