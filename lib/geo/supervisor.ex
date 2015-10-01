aafa334c42f2942d09a23ccca0657fa4372e5697
arpunk <arpunk@cryptolab.net>
2015-09-30 19:52:07 -0500

    Remove unused metadata

--- lib/geo/supervisor.ex
+++ lib/geo/supervisor.ex
@@ -2,12 +2,10 @@ defmodule Geo.Supervisor do
   @moduledoc false
   use Supervisor
 
-  @doc false
   def start_link do
     Supervisor.start_link(__MODULE__, [], name: __MODULE__)
   end
 
-  @doc false
   def init(_args) do
     children = [
       supervisor(Geo.Region.Supervisor, [])
